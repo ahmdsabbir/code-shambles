205f366187638f4acb263f8a890aad048951a55d
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-12-22 13:55:05 +0600

    feat: collapse methods of a class instead of the class itself

--- src/domain/typescript/functionAnalyzer.ts
+++ src/domain/typescript/functionAnalyzer.ts
@@ -12,6 +12,7 @@ export class FunctionAnalyzer {
     const topLevelRanges: ts.TextRange[] = [];
 
     const visit = (node: ts.Node) => {
+      // Get ranges of the top level functions of the page
       if (ts.isFunctionDeclaration(node) && node.body) {
         topLevelRanges.push({
           pos: node.getStart(),
@@ -19,17 +20,29 @@ export class FunctionAnalyzer {
         });
       }
 
-      if (ts.isClassDeclaration(node)) {
+      // Get ranges of the arrow functions
+      if (ts.isArrowFunction(node) && node.body) {
         topLevelRanges.push({
           pos: node.getStart(),
           end: node.getEnd(),
         });
       }
 
-      if (ts.isArrowFunction(node) && node.body) {
-        topLevelRanges.push({
-          pos: node.getStart(),
-          end: node.getEnd(),
+      // Get ranges of the methods of a class
+      if (ts.isClassDeclaration(node) && node.members) {
+        // Methods within a class
+        node.members.forEach((member) => {
+          if (ts.isMethodDeclaration(member) && member.body) {
+            topLevelRanges.push({
+              pos: member.getStart(),
+              end: member.body.getEnd(),
+            });
+          } else if (ts.isConstructorDeclaration(member) && member.body) {
+            topLevelRanges.push({
+              pos: member.getStart(),
+              end: member.body.getEnd(),
+            });
+          }
         });
       }
 
